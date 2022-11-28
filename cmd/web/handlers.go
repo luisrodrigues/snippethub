76dcf9f6f6b4f9169e91e324c67488db76f38efe
Luís Rodrigues <luis.cnm@gmail.com>
2022-11-28 10:39:10 +0000

    Added session management to allow pop-up message when creating new snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -43,7 +43,6 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
 
@@ -93,6 +92,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- go.mod
+++ go.mod
@@ -9,3 +9,8 @@ require github.com/justinas/alice v1.2.0
 require github.com/julienschmidt/httprouter v1.3.0
 
 require github.com/go-playground/form/v4 v4.2.0
+
+require (
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20220528130143-d93ace5be94b // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+)
