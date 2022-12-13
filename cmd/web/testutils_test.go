a0cf61b337603a6f3d121ffffca0566f874c1ed1
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-13 19:45:12 +0000

    - Created mocks package for testing purposes. - Refactored models package to have interfaces - Refactored application to use previous interfaces and allow mock (test) implementations

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"bytes"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form/v4"
 	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"snippethub.luisrodrigues.io/internal/models/mocks"
 	"testing"
+	"time"
 )
 
 func newTestApplication(t *testing.T) *application {
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
+
 	return &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
+		errorLog:       log.New(io.Discard, "", 0),
+		infoLog:        log.New(io.Discard, "", 0),
+		snippets:       &mocks.SnippetModel{},
+		users:          &mocks.UserModel{},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 }
 
