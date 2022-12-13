ee84580ea9a7d8903f297be9b2092247ebe97a4e
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-13 19:33:17 +0000

    Refactored handler tests and testutils to use a test server for storing cookies and don't follow redirects.

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"snippethub.luisrodrigues.io/internal/assert"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	app := newTestApplication(t)
 
-	ping(rr, r)
-	rs := rr.Result()
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-	defer rs.Body.Close()
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
